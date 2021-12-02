3912cd9865b1a2241bf3022f9d4d757c1c666a83
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-12-02 08:24:43 -0300

    post card refactor

--- testes_tecnicos/desafio_james-tip/james/src/layout/Posts/PostCard/PostCard.jsx
+++ testes_tecnicos/desafio_james-tip/james/src/layout/Posts/PostCard/PostCard.jsx
@@ -1,47 +1,28 @@
-import RemoveIcon from "@mui/icons-material/CancelRounded";
-import EditIcon from "@mui/icons-material/EditRounded";
-import {
-  Container,
-  Post,
-  LightText,
-  LightTextMargin,
-  Price,
-  Actions,
-} from "./styles";
-import NumberFormat from "react-number-format";
+import RemoveIcon from '@mui/icons-material/CancelRounded';
+import EditIcon from '@mui/icons-material/EditRounded';
+import { Container } from './styles';
+import NumberFormat from 'react-number-format';
 
-const PostCard = ({
-  key,
-  name,
-  code,
-  category,
-  provider,
-  cost,
-  callRemove,
-  callEdit,
-}) => {
-  return (
-    <Container>
-      <Actions>
-        <RemoveIcon onClick={callRemove} color="error" />
-        <EditIcon onClick={callEdit} color="primary" />
-      </Actions>
-      <Post>
-        <LightTextMargin>Cód.: {code}</LightTextMargin>
-        <h3>{name}</h3>
-        <LightText>Categoria: {category}</LightText>
-        <LightText>Produtor: {provider}</LightText>
-        
-        <Price>
-          <NumberFormat
-            value={cost}
-            displayType="text"
-            thousandSeparator={true}
-            prefix="R$"
-          />
-        </Price>
-      </Post>
-    </Container>
-  );
+const PostCard = ({ key, name, code, category, provider, cost, callRemove, callEdit }) => {
+	return (
+		<Container key={key}>
+			<figure>
+				<RemoveIcon onClick={callRemove} color="error" />
+				<EditIcon onClick={callEdit} color="primary" />
+			</figure>
+			<section>
+				<div>
+					<p>Cód.: {code}</p>
+				</div>
+				<h3>{name}</h3>
+				<p>Categoria: {category}</p>
+				<p>Produtor: {provider}</p>
+
+				<article>
+					<NumberFormat value={cost} displayType="text" thousandSeparator={true} prefix="R$" />
+				</article>
+			</section>
+		</Container>
+	);
 };
 export default PostCard;
--- testes_tecnicos/desafio_james-tip/james/src/layout/Posts/PostCard/styles.jsx
+++ testes_tecnicos/desafio_james-tip/james/src/layout/Posts/PostCard/styles.jsx
@@ -1,33 +1,41 @@
 import styled from 'styled-components';
 import { lightGrey, lightGrey4 } from '../../../styles/colors';
 
-export const Container = styled.div`
+export const Container = styled.main`
 	height: 18rem;
 	width: 14rem;
-  margin:14px;
-  margin-left:2rem;
+	margin: 14px;
+	margin-left: 2rem;
 	background: white;
 	box-shadow: 0 0 10px ${lightGrey4};
 	transition: transform 100mx ease-in-out;
 	font-family: "Nunito", sans-serif;
 
-
 	&:hover {
 		transform: scale(1.05);
 	}
-`;
 
-export const Post = styled.div`padding: 1rem;`;
-export const LightText = styled.p`color: ${lightGrey};`;
-export const LightTextMargin = styled.p`
-	color: ${lightGrey};
-	margin-bottom: 4px;
-`;
+	figure {
+		padding: 1rem;
+		display: flex;
+		justify-content: space-between;
+		cursor: pointer;
+	}
 
-export const Price = styled.p`margin-top: 0.5rem;`;
-export const Actions = styled.div`
-	padding: 1rem;
-	display: flex;
-	justify-content: space-between;
-	cursor: pointer;
+	section {
+		padding: 1rem;
+
+		p {
+			color: ${lightGrey};
+		}
+		article {
+			margin-top: 1rem;
+		}
+		div {
+			p {
+				color: ${lightGrey};
+				margin-bottom: 4px;
+			}
+		}
+	}
 `;
